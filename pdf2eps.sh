e7c968c8389814ccfbe0d70221c7359d2cae6462
Ben Moore <b3nmoore@gmail.com>
2015-05-04 11:17:32 +0100

    indentation, comments

--- pdf2eps.sh
+++ pdf2eps.sh
@@ -6,10 +6,12 @@ imagemagick=true
 gs=true
 pdftops=true
 
+# TODO: allow $1 to be file/directory
+#       - either convert (single file) or iterate through dir
+
 dir="pdf"
 
-for i in `ls $dir/*.pdf`;
-do
+for i in `ls $dir/*.pdf`; do
   b="${i%%.*}"
   echo "\nConverting: $b (Absolute path: `grealpath $i`)"
 
@@ -18,50 +20,46 @@ do
   # OS X install :: http://wiki.inkscape.org/wiki/index.php/MacOS_X#Inkscape_command_line
   #        Notes :: good conversion, egregious trimming
 
-  if [ $inkscape = true ]
-    then
+  if [ $inkscape = true ]; then
     mkdir -p inkscape
     inkscape `grealpath $dir/$i` --export-eps=`grealpath inkscape`/$b.eps
     if [ $? -ne 0 ]; then echo "Inkscape failed" && inkscape=false; fi
-    fi
+  fi
 
 
-    ###  ImageMagick
-    #
-    # OS X install :: brew install imagemagick
-    #        Notes :: nightmarish results
+  ###  ImageMagick
+  #
+  # OS X install :: brew install imagemagick
+  #        Notes :: nightmarish results
 
-    if [ $imagemagick = true ]
-      then
-      mkdir -p imagemagick
-      convert $dir/$i imagemagick/$b.eps
-      if [ $? -ne 0 ]; then echo "ImageMagick failed" && imagemagick=false; fi
-      fi
+  if [ $imagemagick = true ]; then
+    mkdir -p imagemagick
+    convert $dir/$i imagemagick/$b.eps
+    if [ $? -ne 0 ]; then echo "ImageMagick failed" && imagemagick=false; fi
+  fi
 
 
-      ###  ghostscript
-      #
-      # OS X install :: brew install ghostscript
-      #        Notes :: bravo, good conversion + respects PDF bounding
+  ###  ghostscript
+  #
+  # OS X install :: brew install ghostscript
+  #        Notes :: bravo, good conversion + respects PDF bounding
 
-      if [ $gs = true ]
-        then
-        mkdir -p ghostscript
-        gs -q -dNOCACHE -dNOPAUSE -dBATCH -dSAFER -sDEVICE=eps2write -sOutputFile=ghostscript/$b.eps $dir/$i
-        if [ $? -ne 0 ]; then echo "Ghostscript failed" && gs=false; fi
-        fi
+  if [ $gs = true ]; then
+    mkdir -p ghostscript
+    gs -q -dNOCACHE -dNOPAUSE -dBATCH -dSAFER -sDEVICE=eps2write -sOutputFile=ghostscript/$b.eps $dir/$i
+    if [ $? -ne 0 ]; then echo "Ghostscript failed" && gs=false; fi
+  fi
 
-        ###  pdftops
-        #
-        # OS X install :: part of xpdf (binaries at: http://www.foolabs.com/xpdf/download.html)
-        #        Notes :: similar results to ghostscript
+  ###  pdftops
+  #
+  # OS X install :: part of xpdf (binaries at: http://www.foolabs.com/xpdf/download.html)
+  #        Notes :: similar results to ghostscript
 
-        if [ $pdftops = true ]
-          then
-          mkdir -p pdftops
-          pdftops -eps $dir/$i
-          mv $dir/$b.eps pdftops/. 2>/dev/null
-          if [ $? -ne 0 ]; then echo "pdftops failed" && pdftops=false; fi
-          fi
+  if [ $pdftops = true ]; then
+    mkdir -p pdftops
+    pdftops -eps $dir/$i
+    mv $dir/$b.eps pdftops/. 2>/dev/null
+    if [ $? -ne 0 ]; then echo "pdftops failed" && pdftops=false; fi
+  fi
 
-        done
+done
